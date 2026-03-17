9820e57e0de723dfbf0853bcf32d6841a901c064
camellia-ver <jakahi435@gmail.com>
2026-03-17 15:58:41 +0900

    feat: implement AddTransaction function

--- BudgetManager/BudgetManager/Services/DatabaseService.cs
+++ BudgetManager/BudgetManager/Services/DatabaseService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BudgetManager.Models;
 
 namespace BudgetManager.Services
 {
@@ -41,6 +42,26 @@ namespace BudgetManager.Services
 
             using(var cmd = new SqliteCommand(sql, connection))
             {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+
+            string sql = "INSERT INTO Transactions(Amount, Date, Description,Type, Category)" +
+                "VALUES(@Amount, @Date, @Description, @Type, @Category);";
+
+            using(var cmd = new SqliteCommand(sql, connection))
+            {
+                cmd.Parameters.AddWithValue("@Amount", transaction.Amount);
+                cmd.Parameters.AddWithValue("@Date", transaction.Date.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@Description", transaction.Description);
+                cmd.Parameters.AddWithValue("@Type", transaction.Type);
+                cmd.Parameters.AddWithValue("@Category", transaction.Category);
+
                 cmd.ExecuteNonQuery();
             }
         }
