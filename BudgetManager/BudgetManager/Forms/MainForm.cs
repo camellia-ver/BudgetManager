29824ac2e3904507084ac85c51827a2e602a6d99
camellia-ver <jakahi435@gmail.com>
2026-03-19 15:55:36 +0900

    feat: implement LoadMonthlySummary function

--- BudgetManager/BudgetManager/Forms/MainForm.cs
+++ BudgetManager/BudgetManager/Forms/MainForm.cs
@@ -19,11 +19,20 @@ namespace BudgetManager
         private void LoadData()
         {
             LoadBalance();
+            LoadMonthlySummary();
         }
 
         private void LoadBalance()
         {
             lblBalance.Text = $"₩ {_databaseService.GetBalance():N0}";
+        }
+
+        private void LoadMonthlySummary()
+        {
+            DateTime date = dtpMonth.Value;
+            (decimal income, decimal expense) =_databaseService.GetMonthlyTotal(date);
+            lblIncome.Text = $"이번 달 수입: ₩ {income:N0}";
+            lblExpense.Text = $"이번 달 지출: ₩ {expense:N0}";
         }
     }
 }
